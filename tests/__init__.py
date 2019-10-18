5569f5bc26682c9be10f2006b3ed35d8fab8306e
Stein Magnus Jodal <stein.magnus@jodal.no>
2019-10-18 09:58:49 +0200

    Merge branch 'release-2.3' into develop

---
+++ tests/__init__.py
@@ -0,0 +1,40 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+
+from mopidy import compat
+
+
+def path_to_data_dir(name):
+    if not isinstance(name, bytes):
+        name = name.encode('utf-8')
+    if compat.PY2:
+        path = os.path.dirname(__file__)
+    else:
+        path = os.path.dirname(__file__.encode('utf-8'))
+    path = os.path.join(path, b'data')
+    path = os.path.abspath(path)
+    return os.path.join(path, name)
+
+
+class IsA(object):
+
+    def __init__(self, klass):
+        self.klass = klass
+
+    def __eq__(self, rhs):
+        try:
+            return isinstance(rhs, self.klass)
+        except TypeError:
+            return type(rhs) == type(self.klass)  # noqa
+
+    def __ne__(self, rhs):
+        return not self.__eq__(rhs)
+
+    def __repr__(self):
+        return str(self.klass)
+
+
+any_int = IsA(compat.integer_types)
+any_str = IsA(compat.string_types)
+any_unicode = IsA(compat.text_type)
